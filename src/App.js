466447e5825ca1ff87135ea1908216f2608bbabe
aadil42 <adilkelawala@gmail.com>
2023-06-08 12:24:40 +0530

    added dummy homepage with the help of react-routerr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from './pages/Homepage';
+
+const route = createBrowserRouter([
+  { path: '/', element: <Homepage />}
+]);
+
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={route} />;
 }
 
 export default App;
