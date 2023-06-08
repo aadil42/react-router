6ded123fd863cb0b08b91144712036307bbab4b3
aadil42 <adilkelawala@gmail.com>
2023-06-08 13:30:32 +0530

    creating reouteDefinetions with Route components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Products from './pages/Products';
 
-const route = createBrowserRouter([
-  { path: '/', element: <Homepage />},
-  { path: 'products', element: <Products />}
-]);
+const routeDefinetions = createRoutesFromElements(
+  <Route>
+    <Route path="/"  element={<Homepage />} />
+    <Route path="products" element={<Products />} />
+  </Route>
+);
+
+const route = createBrowserRouter(routeDefinetions);
+// const route = createBrowserRouter([
+  // { path: '/', element: <Homepage />},
+  // { path: 'products', element: <Products />}
+// ]);
 
 
 function App() {
