9891475a4995390443796567f069e12446cea37a
aadil42 <adilkelawala@gmail.com>
2023-06-09 15:11:26 +0530

    added link active classes withe help of NavLink  component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import classes from './Navigation.module.css';
 
 const Navigation = () => {
@@ -6,8 +6,21 @@ const Navigation = () => {
         <header className={classes.header}>
             <nav>
                 <ul className={classes.list}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="products">Products</Link></li>
+                    <li>
+                    <NavLink 
+                    to="/" 
+                    className={({isActive}) => {
+                        return isActive ? classes.active : undefined
+                    }} >Home</NavLink>
+                    </li>
+                    <li>
+                    <NavLink 
+                    to="products" 
+                    className={({isActive}) => { 
+                       return isActive ? classes.active : undefined
+                    }}
+                     >Products</NavLink>
+                     </li>
                 </ul>
             </nav>
         </header>
